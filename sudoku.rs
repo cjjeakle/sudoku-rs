569438fee877f5c44889b84039c6d4bc3610ed74
Chris Jeakle <cjjeakle@umich.edu>
2021-12-27 22:17:32 -0500

    Minor formatting and naming tweaks

--- sudoku.rs
+++ sudoku.rs
@@ -34,8 +34,9 @@ Solver
 // Returns true if a solution was found, returns false if the provided state is a dead-end.
 // `starting_i` and `starting_j` are used to skip already solved rows and cols.
 fn search_for_solution(mut state: State, starting_i: usize, starting_j: usize) -> bool {
-    // Continually `find_and_propagate_singletons` while it makes forward progress.
-    while find_and_propagate_singletons(&mut state) {}
+    while find_and_propagate_singletons(&mut state) {
+        // Repeat `find_and_propagate_singletons` while it makes forward progress.
+    }
     if state.unsolved_squares > 0 {
         // If we still haven't solved the board, recursively guess (DFS).
         for i in starting_i..9 {
@@ -57,8 +58,8 @@ fn search_for_solution(mut state: State, starting_i: usize, starting_j: usize) -
                         return true;
                     }
                 }
-                // If there is no viable solution for this square, then we're at a dead-end.
                 if state.board[i][j].solution == 0 {
+                    // If we found no solution for this square, then we're at a dead-end.
                     return false;
                 }
             }
@@ -104,33 +105,33 @@ fn find_and_propagate_singletons(state: &mut State) -> bool {
 
 // Applies solution to the square at offset row, col.
 // Removes solution as a possibility from the square's peers.
-fn propagate_solution(state: &mut State, sln_row: usize, sln_col: usize, solution: i8) {
-    assert!(sln_row < 9);
-    assert!(sln_col < 9);
+fn propagate_solution(state: &mut State, target_row: usize, target_col: usize, solution: i8) {
+    assert!(target_row < 9);
+    assert!(target_col < 9);
     assert!(solution >= 1);
     assert!(solution <= 9);
     assert!(state.unsolved_squares > 0);
     let board = &mut state.board;
-    let sln_val_index = (solution - 1) as usize;
+    let possibility_idx = (solution - 1) as usize;
     // Set the solution.
     state.unsolved_squares -= 1;
-    board[sln_row][sln_col].solution = solution;
+    board[target_row][target_col].solution = solution;
     // Clear option from the row.
     for j in 0..9 {
-        board[sln_row][j].possible[sln_val_index] = false;
+        board[target_row][j].possible[possibility_idx] = false;
     }
     // Clear option from the col.
     for i in 0..9 {
-        board[i][sln_col].possible[sln_val_index] = false;
+        board[i][target_col].possible[possibility_idx] = false;
     }
     // Clear option from the sub-board.
-    let sub_board_row = sub_board_offset(sln_row);
-    let sub_board_col = sub_board_offset(sln_col);
+    let sub_board_row = sub_board_offset(target_row);
+    let sub_board_col = sub_board_offset(target_col);
     for i in 0..3 {
         for j in 0..3 {
             let row = sub_board_row * 3 + i;
             let col = sub_board_col * 3 + j;
-            board[row][col].possible[sln_val_index] = false;
+            board[row][col].possible[possibility_idx] = false;
         }
     }
 }
@@ -147,8 +148,9 @@ fn populate_board_using_input(state: &mut State) {
         Err(error) => println!("error: {}", error),
     }
     let input_bytes: Vec<u8> = input.as_bytes().to_vec();
-    assert_eq!(input_bytes.len(), 82); // 81 squares, plus the null byte
-                                       // Parse input, propagate givens.
+    // 81 squares, plus the null byte.
+    assert_eq!(input_bytes.len(), 82);
+    // Parse input, propagate givens.
     for i in 0..9 {
         for j in 0..9 {
             let cur_byte = input_bytes[i * 9 + j] as u8;
