933d6caaa180d10c2cc9b9ddf6896b74fa6992e2
Chris Jeakle <cjjeakle@umich.edu>
2021-12-27 23:54:56 -0500

    The solver now works on the toughest Sudokus

--- sudoku.rs
+++ sudoku.rs
@@ -24,7 +24,7 @@ fn main() {
     // Populate givens.
     populate_board_using_input(&mut state);
     // Search for a solution.
-    search_for_solution(state, 0, 0);
+    search_for_solution(state);
 }
 
 /*
@@ -33,27 +33,23 @@ Solver
 
 // Returns true if a solution was found, returns false if the provided state is a dead-end.
 // `starting_i` and `starting_j` are used to skip already solved rows and cols.
-fn search_for_solution(mut state: State, starting_i: usize, starting_j: usize) -> bool {
-    while find_and_propagate_singletons(&mut state) {
-        // Repeat `find_and_propagate_singletons` while it makes forward progress.
-    }
+fn search_for_solution(state: State) -> bool {
     if state.unsolved_squares > 0 {
-        // If we still haven't solved the board, recursively guess (DFS).
-        for i in starting_i..9 {
-            for j in starting_j..9 {
+        for i in 0..9 {
+            for j in 0..9 {
                 if state.board[i][j].solution > 0 {
                     // Nothing to do for solved cells.
                     continue;
                 }
-                for possible_idx in 0..9 {
-                    if !state.board[i][j].possible[possible_idx] {
+                for sln_idx in 0..9 {
+                    if !state.board[i][j].possible[sln_idx] {
                         // Skip invalid possibilities.
                         continue;
                     }
                     // Copy state and try the current solution.
                     let mut state_copy = state.clone();
-                    propagate_solution(&mut state_copy, i, j, (possible_idx + 1) as i8);
-                    if search_for_solution(state_copy, i, j) {
+                    propagate_solution(&mut state_copy, i, j, (sln_idx + 1) as i8);
+                    if search_for_solution(state_copy) {
                         // If we found a solution, then we're done!
                         return true;
                     }
@@ -74,35 +70,6 @@ fn search_for_solution(mut state: State, starting_i: usize, starting_j: usize) -
     return false;
 }
 
-// Finds squares with only one possible solution, and sets that as the square's solution.
-// Returns true if any solution as found during this iteration, otherwise false.
-fn find_and_propagate_singletons(state: &mut State) -> bool {
-    // If there are no unsolved squares, then there's nothing to do.
-    if state.unsolved_squares == 0 {
-        return false;
-    }
-    let mut any_squares_solved = false;
-    for i in 0..9 {
-        for j in 0..9 {
-            let mut num_possible = 0;
-            let mut possible_idx = 0;
-            if state.board[i][j].solution == 0 {
-                for sln_idx in 0..9 {
-                    if state.board[i][j].possible[sln_idx] {
-                        num_possible += 1;
-                        possible_idx = sln_idx;
-                    };
-                }
-                if num_possible == 1 {
-                    any_squares_solved = true;
-                    propagate_solution(state, i, j, (possible_idx + 1) as i8)
-                }
-            }
-        }
-    }
-    return any_squares_solved;
-}
-
 // Applies solution to the square at offset row, col.
 // Removes solution as a possibility from the square's peers.
 fn propagate_solution(state: &mut State, target_row: usize, target_col: usize, solution: i8) {
@@ -112,17 +79,21 @@ fn propagate_solution(state: &mut State, target_row: usize, target_col: usize, s
     assert!(solution <= 9);
     assert!(state.unsolved_squares > 0);
     let board = &mut state.board;
-    let possibility_idx = (solution - 1) as usize;
     // Set the solution.
     state.unsolved_squares -= 1;
     board[target_row][target_col].solution = solution;
+    // Clear all possibilities for the target square.
+    for i in 0..9 {
+        board[target_row][target_col].possible[i] = false;
+    }
+    let sln_idx = (solution - 1) as usize;
     // Clear option from the row.
     for j in 0..9 {
-        board[target_row][j].possible[possibility_idx] = false;
+        board[target_row][j].possible[sln_idx] = false;
     }
     // Clear option from the col.
     for i in 0..9 {
-        board[i][target_col].possible[possibility_idx] = false;
+        board[i][target_col].possible[sln_idx] = false;
     }
     // Clear option from the sub-board.
     let sub_board_row = sub_board_offset(target_row);
@@ -131,7 +102,7 @@ fn propagate_solution(state: &mut State, target_row: usize, target_col: usize, s
         for j in 0..3 {
             let row = sub_board_row * 3 + i;
             let col = sub_board_col * 3 + j;
-            board[row][col].possible[possibility_idx] = false;
+            board[row][col].possible[sln_idx] = false;
         }
     }
 }
