e0d9eafbb519ee8a20bb5d86d68b7b11b4927e4a
Chris Jeakle <cjjeakle@umich.edu>
2021-12-29 22:43:12 -0500

    Minor refactor
    Split nested code into a separate method.
    Replaced some if-else statements with cmp::min and cmp::max.

--- sudoku.rs
+++ sudoku.rs
@@ -1,3 +1,4 @@
+use std::cmp;
 use std::io;
 use std::sync::atomic::{AtomicBool, AtomicIsize, Ordering};
 use std::thread;
@@ -111,6 +112,7 @@ impl State {
 
 impl Square {
     fn is_valid(&self) -> bool {
+        // To be valid, squares need a solution or candidate solutions.
         return self.solution > 0 || self.num_possible > 0;
     }
 }
@@ -120,90 +122,30 @@ Solver
 */
 
 // Returns true if a solution was found, returns false if the provided state is a dead-end.
-fn parallel_solve(
-    state: State,
-    skip_to_row: usize,
-    skip_to_col: usize,
-    max_threads: isize,
-) -> bool {
+// Skips past squares before `row`, `col` (in {row,col} order).
+fn parallel_solve(state: State, mut row: usize, mut col: usize, max_threads: isize) -> bool {
     assert!(max_threads > 0);
-    // Initialize to 1, to account for the main thread.
-    static THREAD_QUOTA_IN_USE: AtomicIsize = AtomicIsize::new(1);
     static EXECUTION_CANCELLED: AtomicBool = AtomicBool::new(false);
     // Cancellations are best effort, so use `Ordering::Relaxed`.
     if EXECUTION_CANCELLED.load(Ordering::Relaxed) {
         return false;
     }
-    let mut i = skip_to_row;
-    let mut j = skip_to_col;
     if state.unsolved_squares > 0 {
-        while i < 9 {
-            while j < 9 {
-                if state.board[i][j].solution > 0 {
+        while row < 9 {
+            while col < 9 {
+                if state.board[row][col].solution > 0 {
                     // Nothing to do for solved cells.
-                    j += 1;
+                    col += 1;
                     continue;
                 }
-                if !state.board[i][j].is_valid() {
+                if !state.board[row][col].is_valid() {
                     // If any square is invalid, then this branch is a dead-end.
                     return false;
                 }
-                let threads_needed: isize = state.board[i][j].num_possible.into();
-                // Request threads to branch off and explore possibilities in parallel.
-                // Use `Ordering::SeqCst` to ensure `RUNNING_SOLVER_THREADS` is accurate.
-                let thread_quota_in_use =
-                    THREAD_QUOTA_IN_USE.fetch_add(threads_needed, Ordering::SeqCst);
-                let mut threads_available = if thread_quota_in_use < max_threads {
-                    max_threads - thread_quota_in_use
-                } else {
-                    0
-                };
-                let overflow = if threads_available < threads_needed {
-                    threads_needed - threads_available
-                } else {
-                    0
-                };
-                if overflow > 0 {
-                    THREAD_QUOTA_IN_USE.fetch_add(-overflow, Ordering::SeqCst);
-                }
-                let mut child_threads = vec![];
-                for sln_idx in 0..9 {
-                    if !state.board[i][j].possible[sln_idx] {
-                        // Skip invalid possibilities.
-                        continue;
-                    }
-                    // Copy state and try a candidate solution for this square.
-                    let mut state_copy = state.clone();
-                    state_copy.propagate_solution(i, j, (sln_idx + 1) as i8);
-                    // If there are multiple possibilities to explore, and there are threads available,
-                    // spawn a solver in another thread.
-                    if threads_available > 0 {
-                        threads_available -= 1;
-                        child_threads.push(thread::spawn(move || -> bool {
-                            let solution_found = parallel_solve(state_copy, i, j + 1, max_threads);
-                            THREAD_QUOTA_IN_USE.fetch_add(-1, Ordering::SeqCst);
-                            return solution_found;
-                        }));
-                    } else {
-                        // Decrement if we don't end up kicking off a thread.
-                        if parallel_solve(state_copy, i, j + 1, max_threads) {
-                            // If we found a solution, then we're done!
-                            return true;
-                        }
-                    }
-                }
-                // Wait for all child threads to finish.
-                for thread_handle in child_threads {
-                    if thread_handle.join().unwrap() {
-                        // If any child thread found a solution, then we're done!
-                        return true;
-                    }
-                }
-                // If we found no solution for this square, then the branch we're on is a dead-end.
-                return false;
+                return parallel_solve_impl(state, row, col, max_threads);
             }
-            i += 1;
-            j = 0;
+            row += 1;
+            col = 0;
         }
     } else {
         // Print the solution and cancel other threads.
@@ -214,6 +156,58 @@ fn parallel_solve(
     }
     // We should not get to this point.
     assert!(false);
+    return false;
+}
+
+// Tests solutions for a given square, and recursively searches onward from each candidate solution.
+// Can kick off up to `max_threads` (including the main thread) across all ongoing searches.
+fn parallel_solve_impl(state: State, row: usize, col: usize, max_threads: isize) -> bool {
+    // Initialize to 1, to account for the main thread.
+    static THREAD_QUOTA_IN_USE: AtomicIsize = AtomicIsize::new(1);
+    // Request 1 thread per possibility, don't forget to account for the current thread.
+    let extra_threads_to_request: isize = (state.board[row][col].num_possible - 1).into();
+    // Use `Ordering::SeqCst` to ensure `RUNNING_SOLVER_THREADS` is accurate.
+    let thread_quota_available = cmp::max(
+        max_threads - THREAD_QUOTA_IN_USE.fetch_add(extra_threads_to_request, Ordering::SeqCst),
+        0,
+    );
+    let mut extra_threads_available = cmp::min(thread_quota_available, extra_threads_to_request);
+    let overflow = cmp::max(extra_threads_to_request - extra_threads_available, 0);
+    if overflow > 0 {
+        THREAD_QUOTA_IN_USE.fetch_add(-overflow, Ordering::SeqCst);
+    }
+    let mut child_threads = vec![];
+    for sln_idx in 0..9 {
+        if !state.board[row][col].possible[sln_idx] {
+            // Skip invalid possibilities.
+            continue;
+        }
+        // Copy state and try a candidate solution for this square.
+        let mut state_copy = state.clone();
+        state_copy.propagate_solution(row, col, (sln_idx + 1) as i8);
+        // If there are multiple possibilities to explore, and there are threads available,
+        // spawn a solver in another thread.
+        if extra_threads_available > 0 {
+            extra_threads_available -= 1;
+            child_threads.push(thread::spawn(move || -> bool {
+                let solution_found = parallel_solve(state_copy, row, col + 1, max_threads);
+                THREAD_QUOTA_IN_USE.fetch_add(-1, Ordering::SeqCst);
+                return solution_found;
+            }));
+        } else {
+            // Decrement if we don't end up kicking off a thread.
+            if parallel_solve(state_copy, row, col + 1, max_threads) {
+                // If we found a solution, then we're done!
+                return true;
+            }
+        }
+    }
+    // Wait for all child threads to finish.
+    for thread_handle in child_threads {
+        // If any child thread found a solution, then we're done!
+        if thread_handle.join().unwrap() { return true; }
+    }
+    // If we found no solution for this square, then the branch we're on is a dead-end.
     return false;
 }
 
